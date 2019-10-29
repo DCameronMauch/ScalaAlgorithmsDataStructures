cdfdd49a1deb255622942ffc63c995bc0fc8f98d
D Cameron Mauch <cam@douglas-cameron.net>
2019-10-29 11:50:30 -0600

    changed anagram output type to set of int

--- src/main/scala/com/github/DCameronMauch/ScalaAlgorithmsDataStructures/algorithms/Anagram.scala
+++ src/main/scala/com/github/DCameronMauch/ScalaAlgorithmsDataStructures/algorithms/Anagram.scala
@@ -1,5 +1,5 @@
 package com.github.DCameronMauch.ScalaAlgorithmsDataStructures.algorithms
 
 object Anagram {
-  def contains(source: String, pattern: String): Boolean = false
+  def contains(source: String, pattern: String): Set[Int] = Set.empty
 }
--- src/test/scala/com/github/DCameronMauch/ScalaAlgorithmsDataStructures/algorithms/AnagramSpec.scala
+++ src/test/scala/com/github/DCameronMauch/ScalaAlgorithmsDataStructures/algorithms/AnagramSpec.scala
@@ -6,27 +6,27 @@ class AnagramSpec extends FlatSpec with Matchers {
   val source1 = "abcabcabc"
   val source2 = "abcdefghi"
 
-  "given a direct substring" should "return true" in {
-    Anagram.contains(source1, "abc") shouldBe true
-    Anagram.contains(source2, "def") shouldBe true
+  "given a direct substring" should "return set of indexes" in {
+    Anagram.contains(source1, "abc") shouldBe Set(0, 1, 2, 3, 4, 5, 6)
+    Anagram.contains(source2, "def") shouldBe Set(3)
   }
 
-  "given an out of order substring" should "return true" in {
-    Anagram.contains(source1, "cab") shouldBe true
-    Anagram.contains(source2, "efd") shouldBe true
+  "given an out of order substring" should "return set of indexes" in {
+    Anagram.contains(source1, "cab") shouldBe Set(0, 1, 2, 3, 4, 5, 6)
+    Anagram.contains(source2, "efd") shouldBe Set(3)
   }
 
-  "given a pattern that does not exist in the source regardless of order" should "return false" in {
-    Anagram.contains(source1, "aba") shouldBe false
-    Anagram.contains(source2, "deh") shouldBe false
+  "given a pattern that does not exist in the source regardless of order" should "return empty set" in {
+    Anagram.contains(source1, "aba") shouldBe Set.empty
+    Anagram.contains(source2, "deh") shouldBe Set.empty
   }
 
-  "given an empty source" should "return false" in {
-    Anagram.contains("", "abc") shouldBe false
+  "given an empty source" should "return empty set" in {
+    Anagram.contains("", "abc") shouldBe Set.empty
   }
 
-  "given an empty pattern" should "return false" in {
-    Anagram.contains(source1, "") shouldBe false
-    Anagram.contains(source2, "") shouldBe false
+  "given an empty pattern" should "return empty set" in {
+    Anagram.contains(source1, "") shouldBe Set.empty
+    Anagram.contains(source2, "") shouldBe Set.empty
   }
 }
